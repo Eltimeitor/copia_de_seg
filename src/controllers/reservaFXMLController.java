cf05917fbdb5afe35b02914a0d371ad6a89f2e8c
Eltimeitor <david@DESKTOP-057AUJM>
2023-05-15 16:23:16 +0200

    reserva sin la fecha

--- src/controllers/reservaFXMLController.java
+++ src/controllers/reservaFXMLController.java
@@ -4,14 +4,20 @@
  */
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import model.Club;
+import model.ClubDAOException;
+import model.Member;
 
 /**
  * FXML Controller class
@@ -56,17 +62,61 @@ public class reservaFXMLController implements Initializable {
     private ImageView pista14;
     @FXML
     private ImageView pista15;
+    
+    private Club club;
+    private String login;
+    private String contra;
+    private Member user;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            club.getInstance();
+        } catch (ClubDAOException ex) {
+            Logger.getLogger(reservaFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(reservaFXMLController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        
+        
+        
     }    
 
     @FXML
     private void reservar(ActionEvent event) {
+        
+    }
+    
+    public void init(String log, String pass){
+        this.login = log;
+        this.contra = pass;
+        user = club.getMemberByCredentials(login, contra);
+    }
+
+    @FXML
+    private void reservar2(ActionEvent event) {
+    }
+
+    @FXML
+    private void reservar3(ActionEvent event) {
+    }
+
+    @FXML
+    private void reservar4(ActionEvent event) {
+    }
+
+    @FXML
+    private void reservar5(ActionEvent event) {
+    }
+
+    @FXML
+    private void reservar6(ActionEvent event) {
     }
     
 }
